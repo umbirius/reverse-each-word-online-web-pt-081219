5dd9c2fbef43a78c5fc6633e319c50de09fb1e83
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 20:57:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,19 @@ def reverse_each_word (string)
 string = string.split (/ /)
 fin_string = []
 
+<<<<<<< HEAD
 string.collect do |rev|
   fin_string << rev.reverse! 
 end 
 
 p string.join(" ")
+=======
+string.each do |rev|
+  fin_string << rev.reverse! 
+end 
+
+print fin_string.join(" ")
+>>>>>>> 341380a45bd7466a2c49260e5ba13e38151d15d4
 end 
 
 reverse_each_word("Whee do i doo")
